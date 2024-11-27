31475ff3996d6d790ab206bc0ea6448986800ff9
VariusCasta <149559980+VariusCasta@users.noreply.github.com>
2024-11-27 11:51:05 +0300

    Update convert_txt_to_rsc.ps1

--- convert_txt_to_rsc.ps1
+++ convert_txt_to_rsc.ps1
@@ -1,2 +1,2 @@
 "/ip firewall address-list" | Out-File ./emerging-Block-IPs.rsc
-Get-Content .\emerging-Block-IPs.txt | Where-Object { $_ -notmatch '#' } | ? {$_.trim() -ne "" } | ForEach-Object{ 'add address=' + ($_ -split "\s+")[0] + ' comment="RUSSIAN FEDERATION" list=BlockList'} | Add-content ./emerging-Block-IPs.rsc
+Get-Content .\emerging-Block-IPs.txt | Where-Object { $_ -notmatch '#' } | ? {$_.trim() -ne "" } | ForEach-Object{ 'add address=' + ($_ -split "\s+")[0] + ' comment="Bot block list" list=BlockList'} | Add-content ./emerging-Block-IPs.rsc
