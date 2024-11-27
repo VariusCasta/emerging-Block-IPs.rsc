b735d3bdb972093df91de34e6cb9cb1cbde5d63c
VariusCasta <149559980+VariusCasta@users.noreply.github.com>
2024-11-27 11:52:34 +0300

    Update convert_txt_to_rsc.ps1

--- convert_txt_to_rsc.ps1
+++ convert_txt_to_rsc.ps1
@@ -1,2 +1,3 @@
+wget https://rules.emergingthreats.net/fwrules/emerging-Block-IPs.txt -OutFile emerging-Block-IPs.txt
 "/ip firewall address-list" | Out-File ./emerging-Block-IPs.rsc
 Get-Content .\emerging-Block-IPs.txt | Where-Object { $_ -notmatch '#' } | ? {$_.trim() -ne "" } | ForEach-Object{ 'add address=' + ($_ -split "\s+")[0] + ' comment="Bot block list" list=BlockList'} | Add-content ./emerging-Block-IPs.rsc
